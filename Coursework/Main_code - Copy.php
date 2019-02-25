73398ad5f9bd2b7feb660e39c9f926f39d971db9
c23-day <christopher4.day@live.uwe.ac.uk>
2019-02-25 16:29:26 +0000

    All weather data for one day added + OMG new colours

--- Coursework/Main_code - Copy.php
+++ Coursework/Main_code - Copy.php
@@ -1,14 +1,53 @@
-<!DOCTYPE html>
-<? 
-$url = "http://api.openweathermap.org/data/2.5/find?q=Vancouver&units=metric&type=accurate&mode=xml&APPID=e7f136afc12e538a55ec4e4c0c9722c1";
-$getweather = simplexml_load_file($url);
+<?php
+  function curl($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+  }    
+    
+  $VweatherURL = curl("https://api.openweathermap.org/data/2.5/weather?q=vancouver,ca&appid=e7f136afc12e538a55ec4e4c0c9722c1");
+  $EweatherURL = curl("https://api.openweathermap.org/data/2.5/weather?q=edinburgh,gb&appid=e7f136afc12e538a55ec4e4c0c9722c1");
+  
+  $VweatherArray = json_decode($VweatherURL, true);
+  $EweatherArray = json_decode($EweatherURL, true);
+
+  $VweatherMain = $VweatherArray['weather'][0]['main'];
+  $EweatherMain = $EweatherArray['weather'][0]['main'];
+
+  $VweatherDesc = $VweatherArray['weather'][0]['description'];
+  $EweatherDesc = $EweatherArray['weather'][0]['description'];
+
+  $VtempInCelcius = $VweatherArray['main']['temp'] - 273.15;
+  $EtempInCelcius = $EweatherArray['main']['temp'] - 273.15;
+
+  $VtempInCelciusMin = $VweatherArray['main']['temp_min'] - 273.15;
+  $EtempInCelciusMin = $EweatherArray['main']['temp_min'] - 273.15;
+
+  $VtempInCelciusMax = $VweatherArray['main']['temp_max'] - 273.15;
+  $EtempInCelciusMax = $EweatherArray['main']['temp_max'] - 273.15;
+
+  $VweatherHumid = $VweatherArray['main']['humidity'];
+  $EweatherHumid = $EweatherArray['main']['humidity'];
+
+  $VweatherWind = $VweatherArray['wind']['speed'];
+  $EweatherWind = $EweatherArray['wind']['speed'];
+
+  //print_r($VweatherArray);
+  //print_r($EweatherArray);
 
 ?>
+
+<!DOCTYPE html>
 <html>
   <head>
     <link rel="stylesheet" href="layout.css" type="text/css">
-    <link rel="api" href="api.js">
   </head>
+
 <body>
 
   <table id="vancouver_table" class="inline_table">
@@ -17,20 +56,16 @@ $getweather = simplexml_load_file($url);
     </tr> 
 
     <td><iframe
-      width="600"
+      width="500"
       height="450"
       frameborder="100" style="border:100"
       src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBEwVa1GKs4lLB4CVi4YQ2Ptaep70MV_x8 &q= Vancouver+BC" allowfullscreen>
     </iframe></td>
 
     <td>
-    <iframe
-    width="600"
-    height="200"
-    frameborder="100" style="border:100"
-    src="http://api.openweathermap.org/data/2.5/find?q=Vancouver&units=metric&type=accurate&mode=xml&APPID=e7f136afc12e538a55ec4e4c0c9722c1">
-    </iframe></td>
-
+        <h3>Vancouver Weather</h3>
+        <?php echo "Main Weather: ".($VweatherMain).": (".($VweatherDesc)."),  Temp: ".($VtempInCelcius)." °C, Temp lows: ".($VtempInCelciusMin)." °C, Temp Highs: ".($VtempInCelciusMax)." °C, Humidity: ".($VweatherHumid).", Wind Speeds: ".($VweatherWind)." Mph"?>
+    </td>
   </table>
 
   <table id="edinburgh_table" class="inline_table"> 
@@ -38,7 +73,7 @@ $getweather = simplexml_load_file($url);
         <th>Edinburgh</th>
       </tr>
     <td><iframe
-      width="600"
+      width="500"
       height="450"
       frameborder="100" style="border:100"
       src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBEwVa1GKs4lLB4CVi4YQ2Ptaep70MV_x8 &q= Edinburgh+GB" allowfullscreen>
@@ -46,13 +81,11 @@ $getweather = simplexml_load_file($url);
 
     <td>
         <h3>Edinburgh Weather</h3>
-        <p><? echo($getweather);?></p>
+        <?php echo "Main Weather: ".($EweatherMain).": (". ($EweatherDesc)."),  Temp: ".($EtempInCelcius)."°C, Temp lows: ".($EtempInCelciusMin)." °C, Temp Highs: ".($EtempInCelciusMax)." °C, Humidity: ".($EweatherHumid).", Wind Speeds: ".($EweatherWind)." Mph"?>
     </td>
-
   </table>
 
   <div id="googleMap" style="width:100%;height:400px;"></div>
-  <div id="OpenWeather" style="width:100%; height:400px;"></div>
 
   <script>
     function myMap() {
@@ -64,12 +97,5 @@ $getweather = simplexml_load_file($url);
   }
   </script>
 
-  <script>
-    function myWeather(){
-      var URL="http://api.openweathermap.org/data/2.5/find?q=Vancouver&units=metric&type=accurate&mode=xml&APPID=e7f136afc12e538a55ec4e4c0c9722c1";
-      
-      var weather = new weather.api(documnet.getElementById("OpenWeather"),URL);
-    }
-  </script>
 </body>
 </html>
